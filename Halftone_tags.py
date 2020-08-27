412c361a6cc95308f4bafaa9eef093da6000b3d4
pyhop <70246950+pyhop@users.noreply.github.com>
2020-08-27 14:08:32 -0600

    Update Halftone_tags.py

--- Halftone_tags.py
+++ Halftone_tags.py
@@ -1,24 +1,38 @@
-"""Halftone Tags.
+"""Halftone Tags
+Halftone tags referencing exsiting phased elements
 
-Note:
-Halftone tags referencing 
-existing phased elements 
 """
 __title__ = 'Halftone\nTags' 
 __author__= 'marentette'
-##Import Libraries 
+
 import Autodesk
 from Autodesk.Revit.DB import *
 from Autodesk.Revit.UI import *
+
+from june import revit_transaction 
+#Active document 
 doc = __revit__.ActiveUIDocument.Document
 
-"""Define Result Window"""
+#Result Window
 def window(numberoftags):
     dialog = TaskDialog("Header")
     dialog.MainInstruction = "Results"
     dialog.MainContent = "{0} tags were switched to {1}".format(numberoftags,'halftone')
     dialog.CommonButtons = TaskDialogCommonButtons.Close;
     return dialog.Show()
+
+#Halftone function 
+@revit_transaction('Halftone Existing Tags')
+def halftone(tags): 
+    graphic_halftone = OverrideGraphicSettings().SetHalftone(True)
+    count = 0
+    for t in tags:
+        try: 
+            t.tag_view.SetElementOverrides(t.ID,graphic_halftone)
+            count +=1 
+        except:
+             pass 
+    return count 
 
 """Create Tag Class"""
 class Tag:
@@ -32,38 +46,29 @@ class Tag:
         return view  
     
     @property
-    def phase(self):
-        element = doc.GetElement(self.tag.TaggedLocalElementId)
-        phase = element.get_Parameter(BuiltInParameter.PHASE_CREATED).AsValueString()
-        return True if phase == 'Existing' else False   
+    def el_phase(self):
+        try:
+            element = doc.GetElement(self.tag.TaggedLocalElementId)
+            phase = element.get_Parameter(BuiltInParameter.PHASE_CREATED).AsValueString()
+            existing = True if phase == 'Existing' else False 
+        except:
+            existing = False 
+        return existing  
     
     @property    
     def ID(self):
         return self.tag.Id 
 
-"""Collect Duct, Pipe, and Mechanical Equipment Tags"""
+
+"""Collect Duct, Pipe, and Equipment Tags"""
 duct_tag = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_DuctTags).WhereElementIsNotElementType().ToElements()
 pipe_tag = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_PipeTags).WhereElementIsNotElementType().ToElements()
 equipment_tag = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_MechanicalEquipmentTags).WhereElementIsNotElementType().ToElements()
-
 #Join tags into one list
 all_tags = [Tag(i) for i in duct_tag] + [Tag(i) for i in pipe_tag] + [Tag(i) for i in equipment_tag]
 
-# Filter tags to ones referencing existing elements
-ex_tags = [i for i in all_tags if i.phase]
+# Filter to tags referencing existing elements
+ex_tags = [i for i in all_tags if i.el_phase]
 
-#Create Graphic Settings for Halftone Tags
-graphic_halftone = OverrideGraphicSettings().SetHalftone(True)
-
-#Start Transaction 
-t1 = Transaction(doc, 'Halftone Tags')
-t1.Start()
-count = 0
-for t in ex_tags:
-    try: 
-        t.tag_view.SetElementOverrides(t.ID,graphic_halftone)
-        count +=1 
-    except:
-         pass 
-t1.Commit()
-Result_window = window(count)
+#Result Window
+Result_window = window(halftone(ex_tags))
