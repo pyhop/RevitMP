f48479a163bf8971a223e87fdbbfdf5ec191389b
pyhop <70246950+pyhop@users.noreply.github.com>
2020-12-09 09:17:12 -0700

    Update script.py

--- Align Legends/script.py
+++ Align Legends/script.py
@@ -50,28 +50,18 @@ def legend_views(viewports):
         else: pass
     return Legends
 
-@revit_transaction('Align Legends')
-def move_legend(selected_point,legends_to_move):
-    """Align Legend on Sheet"""
-    for L in legends_to_move:
-        vector = selected_point.Subtract(L.GetBoxOutline().MaximumPoint)
-        new_center = L.GetBoxCenter().Add(vector)
-        new_location = L.SetBoxCenter(new_center)
-                
-if __name__ == '__main__':
-     
-    #namedtuples
+def select_legends(): 
+    """Collect and Filter Viewports in Model/User Selected Legends to Move"""
     viewports = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Viewports).WhereElementIsNotElementType().ToElements()   
     legends = legend_views(viewports) 
     
-    #User Selected Legend
-    selected_legend = selected_legend()
-    if selected_legend == -1:
+    sel_legend = selected_legend()
+    if sel_legend == -1:
         forms.alert('You did not select a legend on the sheet, please try again', title='Error')
         sys.exit() 
     else:
-        selected_name = doc.GetElement(selected_legend.ViewId).Name
-        selected_point = selected_legend.GetBoxOutline().MaximumPoint
+        selected_name = doc.GetElement(sel_legend.ViewId).Name
+        selected_point = sel_legend.GetBoxOutline().MaximumPoint
         
         #Ask User to Select Sheets to Move Legend On
         same_legends = [L for L in legends if L.legend_name == selected_name]
@@ -83,7 +73,22 @@ if __name__ == '__main__':
                                                     title= 'Select Sheets To Move Legend Ons', 
                                                     button_name='Align Legends') 
         
-        legends_to_align = [option_dict[s] for s in selected_sheets if s in option_dict] 
-        
-        #Align Legends
-        move = move_legend(selected_point,legends_to_align)
+        legends_to_align =[option_dict[s] for s in selected_sheets if s in option_dict]
+    
+    return legends_to_align,selected_point 
+
+@revit_transaction('Align Legends')
+def move_legend():
+    """Align Legend on Sheet"""
+    legends= select_legends()
+    legends_to_move = legends[0]
+    selected_point = legends[1]
+    for L in legends_to_move:
+        vector = selected_point.Subtract(L.GetBoxOutline().MaximumPoint)
+        new_center = L.GetBoxCenter().Add(vector)
+        new_location = L.SetBoxCenter(new_center)
+                
+if __name__ == '__main__': 
+    move = move_legend()
+    
+    
