33ee35b7aac3881e778bd06b9ca31632609b3a78
pyhop <70246950+pyhop@users.noreply.github.com>
2021-01-16 16:59:03 -0700

    Update tag_terminals.py

--- Tag Terminals/tag_terminals.py
+++ Tag Terminals/tag_terminals.py
@@ -1,10 +1,8 @@
-"""This script tags any selected elements that are 
-air terminals
+"""This script tags any selected elements or groups and tags
+all the air terminals
 """
-__title__ = 'Tag Terminals'
+__title__ = 'Tag\nTerminals'
 __author__= 'marentette'
-
-import sys 
 
 from Autodesk.Revit.DB import \
 FilteredElementCollector,BuiltInCategory,\
@@ -13,36 +11,39 @@ Reference,TagOrientation
 from Autodesk.Revit.DB.IndependentTag import Create
 
 from june import revit_transaction
-from pyrevit import forms
-
 
 uidoc = __revit__.ActiveUIDocument 
 doc = uidoc.Document
 
-
 def exit_(reason): 
     """Exit Function"""
+    import sys
+    from pyrevit import forms
     forms.alert(reason)
     return sys.exit() 
 
 def selected_terminals():
     """Grab User Selections and Exit if No Air Terminals were Selected"""
-    selected_elements = uidoc.Selection.GetElementIds()
+    selected_elements = uidoc.Selection.GetElementIds()    
     if len(selected_elements) > 0: pass
-    
-    else: exit_('No Elements were Selected')
-    
+    else: 
+        exit_('No Elements were Selected') 
     air_terminals = [] 
     for e in selected_elements: 
         el = doc.GetElement(e)
-        if el.Category.Name == 'Air Terminals':
+        if el.ToString() == 'Autodesk.Revit.DB.Group': #Check if user selected a group 
+            group_members = el.GetMemberIds()
+            for g in group_members:
+                element = doc.GetElement(g)
+                if element.Category.Name== 'Air Terminals':
+                    air_terminals.append(element) 
+        elif el.Category.Name == 'Air Terminals':
             air_terminals.append(el)
         else: pass 
     return air_terminals if air_terminals else exit_('No Air Terminals were Selected')
 
 @revit_transaction('Tag Terminals')
 def tag_terminals(air_terminals):
-    """Tag Terminals"""
     terminal_tags = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_DuctTerminalTags).WhereElementIsNotElementType().ToElements()
     for a in air_terminals:
         Create(doc,
@@ -55,4 +56,3 @@ def tag_terminals(air_terminals):
                    
 if __name__ == '__main__':
     tag_terminals(selected_terminals()) 
-    
